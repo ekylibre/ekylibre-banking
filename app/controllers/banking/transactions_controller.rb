1bf8c8d807a2e9540869f6c614af52143d810b3e
David JOULIN <djoulin@ekylibre.com>
2022-01-27 15:48:39 +0100

    Fix redirect url

--- app/controllers/banking/transactions_controller.rb
+++ app/controllers/banking/transactions_controller.rb
@@ -2,13 +2,13 @@ require 'securerandom'
 
 module Banking
   class TransactionsController < Backend::BaseController
-
+    include Rails.application.routes.url_helpers
     def build_requisition
       cash_id = params[:cash_id]
       bank_service = Banking::Account.new(cash_id: cash_id)
       institution = bank_service.get_institutions
       uuid = SecureRandom.uuid
-      redirect_url = 'http://tim.ekylibre.lan:3000/backend/cashes/1'
+      redirect_url = self.request.base_url + '/backend/cashes/' + cash_id.to_s
       requisition = bank_service.create_requisition(redirect_url: redirect_url, institution_id: institution.id, reference_id: uuid, max_historical_days: 90)
       session[:requisition_id] = requisition.id
       redirect_to requisition.link
