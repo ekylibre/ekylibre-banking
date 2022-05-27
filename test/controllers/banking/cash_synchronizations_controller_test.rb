8afe55556b105a5078ee25f140070584a8fabbf7
Thibaut <tgorioux@ekylibre.com>
2022-05-27 08:47:15 +0200

    Clean controller test

--- test/controllers/banking/cash_synchronizations_controller_test.rb
+++ test/controllers/banking/cash_synchronizations_controller_test.rb
@@ -5,29 +5,32 @@ module Banking
   class CashSynchronizationsControllerTest < Ekylibre::Testing::ApplicationControllerTestCase::WithFixtures
     setup_sign_in
 
+    setup do 
+      @cash = cashes(:cashes_001)
+      cash.update(bank_identifier_code: 'TRWIGB22')
+    end
+
+    attr_reader :cash
+
     test '#new when params are not provided, return a list of all institution' do
       get :new, params: {}
       assert_response :success
-      assert JSON.parse(@controller.instance_variable_get(:@list)).count > 0
+      assert JSON.parse(@controller.instance_variable_get(:@list)).count > 0 , 'Assigns an institution list'
     end
 
     test '#new when cash has bank_identifier_code setted' do
-      cash = cashes(:cashes_001)
       get :new, params: { cash_id: cash.id }
-      assert_redirected_to build_requisition_banking_cash_synchronization_path(cash_id: cash.id)
+      assert_redirected_to build_requisition_banking_cash_synchronization_path(cash_id: cash.id), 'Redirects to build requisition'
     end
 
     test '#build_requisition' do
-      cash = cashes(:cashes_001)
       institution_id = "WISE_TRWIGB22"
       get :build_requisition, params: { cash_id: cash.id, institution_id: institution_id }
       assert_response :redirect
-      assert_not_nil Preference.get("requisition_id_cash_id_#{cash.id}").value
+      assert_not_nil Preference.get("requisition_id_cash_id_#{cash.id}").value, 'Stores the requisition_id in preference'
     end
 
     test '#perform' do
-      cash = cashes(:cashes_001)
-
       assert_raises StandardError do
         get :perform, params: { cash_id: cash.id }
       end
@@ -40,7 +43,7 @@ module Banking
 
       get :perform, params: { cash_id: cash.id }
       ::Banking::BankingFetchUpdateTransactionsJob.verify
-      assert_redirected_to backend_cash_path(cash)
+      assert_redirected_to backend_cash_path(cash), 'Redirects to cash show view'
     end
 
   end
