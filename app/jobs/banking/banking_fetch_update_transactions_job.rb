5b1eecf02fff3ddbe5b3e750aa8466946d0ec67a
Thibaut <tgorioux@ekylibre.com>
2022-05-30 12:07:52 +0200

    Test transaction job

--- app/jobs/banking/banking_fetch_update_transactions_job.rb
+++ app/jobs/banking/banking_fetch_update_transactions_job.rb
@@ -4,24 +4,30 @@ module Banking
     queue_as :default
     VENDOR = 'nordigen'
 
-    def perform(cash_id:, requisition_id:)
+    def perform(cash_id:, requisition_id:, nordigen_service: ::Banking::NordigenService.instance)
       begin
-        nordign_service = ::Banking::NordigenService.instance
-        accounts = nordign_service.get_requisition_accounts(requisition_id)
-        accounts.each do |account|
-          if cash = Cash.find_by(iban: account.iban)
-            cash.provider = { vendor: VENDOR, data: { id: account.id.to_s  } } if cash.provider.blank?
-            cash.save!
-          end
-        end
+        accounts = nordigen_service.get_requisition_accounts(requisition_id)
+        update_cash_provider(accounts)
+
         account_uuid = Cash.find_by(id: cash_id).provider_data[:id]
-        transactions = nordign_service.get_account_transactions(account_uuid: account_uuid)
-        bs_service = ::Banking::BankTransaction.new(cash_id: cash_id)
-        bs_service.import_bank_statements(transactions: transactions)
+        if account_uuid.present?
+          transactions = nordigen_service.get_account_transactions(account_uuid: account_uuid)
+          bs_service = ::Banking::BankTransaction.new(cash_id: cash_id)
+          bs_service.import_bank_statements(transactions: transactions)
+        end
       rescue StandardError => error
         Rails.logger.error $ERROR_INFO
         Rails.logger.error $ERROR_INFO.backtrace.join("\n")
         ExceptionNotifier.notify_exception($ERROR_INFO, data: { message: error })
+      end
+    end
+
+    def update_cash_provider(accounts)
+      accounts.each do |account|
+        if cash = Cash.find_by(iban: account.iban)
+          cash.provider = { vendor: VENDOR, data: { id: account.id.to_s  } } if cash.provider.blank?
+          cash.save!
+        end
       end
     end
   end
