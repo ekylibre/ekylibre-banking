6702f811c661a8a03f764fe56e506f2979864662
Thibaut <tgorioux@ekylibre.com>
2022-06-20 11:53:29 +0200

    Extract each step into method

--- app/jobs/banking/fetch_update_transactions_job.rb
+++ app/jobs/banking/fetch_update_transactions_job.rb
@@ -7,15 +7,11 @@ module Banking
 
     def perform(cash_id:, requisition_id:, nordigen_service: ::Banking::NordigenService.instance)
       begin
+        cash = cash.find(cash_id)
         accounts = nordigen_service.get_requisition_accounts(requisition_id)
-        update_cash_provider(accounts)
-
-        account_uuid = Cash.find_by(id: cash_id).provider_data[:id]
-        if account_uuid.present?
-          transactions = nordigen_service.get_account_transactions(account_uuid: account_uuid)
-          bs_service = ::Banking::BankTransaction.new(cash_id: cash_id)
-          bs_service.import_bank_statements(transactions: transactions)
-        end
+        check_iban(cash, accounts)
+        update_cash_provider(cash, accounts)
+        import_bank_statements(cash.reload, nordigen_service)
       rescue StandardError => error
         Rails.logger.error $ERROR_INFO
         Rails.logger.error $ERROR_INFO.backtrace.join("\n")
@@ -23,14 +19,41 @@ module Banking
       end
     end
 
-    def update_cash_provider(accounts)
-      accounts.each do |account|
-        cash = Cash.find_by(iban: account.iban)
-        if cash
-          cash.provider = { vendor: VENDOR, data: { id: account.id.to_s  } } if cash.provider.blank?
+    private
+
+      # Multiple account can be selected for synchronization, so we need to check that at 
+      # least one account matches cash account (using iban)
+      def check_iban(cash, accounts)
+        unless accounts.map(&:iban).include(cash.iban)
+          raise StandardError.new(:none_account_iban_match_cash_iban.tl(iban: cash.iban))
+        end
+      end
+
+      def update_cash_provider(cash, accounts)
+        account = accounts.select {|account| account.iban == cash.iban }.first
+        if cash.provider.blank?
+          cash.provider = provider_for_account(account)
           cash.save!
         end
       end
-    end
+
+      def import_bank_statements(cash, nordigen_service)
+        account_uuid = cash.provider_data[:id]
+        return if account_uuid.nil?
+
+        transactions = nordigen_service.get_account_transactions(account_uuid: account_uuid)
+        ::Banking::BankTransaction.new(cash_id: cash.id).import_bank_statements(transactions: transactions)
+      end
+
+      def provider_for_account(account)
+        { 
+          vendor: VENDOR, 
+          data: { 
+            id: account.id.to_s,
+            name: 'account'
+          }
+        }
+      end
+
   end
 end
